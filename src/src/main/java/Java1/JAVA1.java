f19bd4232f07f0a491a53f656d336c2f4f05b846
vasantvernekarvr-cmd <vasantvernekar.vr@gmail.com>
2025-09-28 11:51:01 +0530

    Commit file

---
+++ src/src/main/java/Java1/JAVA1.java
@@ -0,0 +1,12 @@
+package Java1;
+
+public class JAVA1 {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int c;
+
+        c = a + b;
+        System.out.println(c);
+    }
+}
